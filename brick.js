c7840f13b31cb1636499ad6e5fea06ef9790ebd5
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-05-27 12:10:23 +0200

    Re-order the code and try to add Win function

--- brick.js
+++ brick.js
@@ -6,8 +6,8 @@
   context = canvas.getContext('2d');
 
   //Constante du jeux qui ne changeront jamais
-  var nb_ligne=7,
-  nb_brick_ligne=10,
+  var nb_ligne=1,
+  nb_brick_ligne=1,
   brick_width=45,
   brick_height=15,
   space_brick=5,
@@ -15,7 +15,7 @@
   barre_height=10,
   barreX,
   barreY,
-  deplacement=20,
+  deplacement=30,
   airzone_width=canvas.width,
   airzone_height=canvas.height,
   couleur= ["#e74c3c", "#c0392b", "#d35400", "#e67e22", "#1abc9c"],
@@ -27,21 +27,34 @@
   dirBalleY=-1,
   aGagne=0,
   bricks=(space_brick+brick_height)*nb_ligne,
-  tab_brick=new Array(nb_ligne);
+  tab_brick=new Array(nb_ligne),
+  PartieEnCours=true;
 
-  //On place la barre au centre du jeu
-  barreX = (airzone_width/2)-(barre_width/2);
-  //en bas
-  barreY = (airzone_height-barre_height);
-  context.fillStyle="white";
-  context.fillRect(barreX,barreY,barre_width,barre_height);
+  function Start(){
+    PartieEnCours=false;
+    //On place la barre au centre du jeu
+    barreX = (airzone_width/2)-(barre_width/2);
+    //en bas
+    barreY = (airzone_height-barre_height);
+    context.fillStyle="white";
+    context.fillRect(barreX,barreY,barre_width,barre_height);
 
-  //On appele la fonction pour generer les briques
-  creation(context,nb_ligne,nb_brick_ligne,brick_width,brick_height,space_brick);
+    //On appele la fonction pour generer les briques
+    creation(context,nb_ligne,nb_brick_ligne,brick_width,brick_height,space_brick);
 
-  var interval = setInterval(Game,10);
-  window.document.onkeydown = checkDepla;
+    var interval = setInterval(Game,10);
+    window.document.onkeydown = checkDepla;
 
+  }
+
+  if(PartieEnCours==true){
+    window.addEventListener('keydown',function(e){
+    if(e.which==32){
+      Start();
+      PartieEnCours==false;
+    }
+  },false)
+  }
 
   function Game(){
     context.clearRect(0,0,airzone_width,airzone_height);
@@ -49,7 +62,7 @@
     context.fillStyle="#34495e";
     context.fillRect(0,0,airzone_width,airzone_height);
     context.fill();
-		// Réaffichage des briques
+    // Réaffichage des briques
     for (var i=0; i<tab_brick.length;i++){
       context.fillStyle=couleur[i];
       for(var j=0; j<tab_brick[i].length;j++){
@@ -59,47 +72,44 @@
         }
       }
     }
-
+    // Si aGagne=0 donc qu'il y a plus de brick dans la tableau on affiche la fonction gagne()
+    if ( aGagne ){
+      gagne();
+    }
     context.fillStyle="white";
     context.fillRect(barreX,barreY,barre_width,barre_height);
+    mainLogic();
+    Colision();
+    Balle();
+  }
 
+  function mainLogic(){
 
-    // on fait bouger la balle en lui ajoutant sa vitesse comme Flappy
-    //On verifie que la balle ne depasse pas de la largeur du canvas
     if( (balleX+dirBalleX*balle_vitesse) > airzone_width){
-      dirBalleX=-1;
+      dirBalleX=-1.2;
     }
     else if( (balleX+dirBalleX*balle_vitesse) < 0 ){
-      dirBalleX=1;
+      dirBalleX=1.2;
     }
-    //On verifie que la balle ne depasse pas de la hauteur du canvas
+    //On verifie que la balle ne touche pas le bas du canvas
     if( (balleY+dirBalleY*balle_vitesse)>airzone_height ){
       perdu();
     }
     else {
-			if ( (balleY + dirBalleY * balle_vitesse) <  0){
-        dirBalleY = 1;
+      if ( (balleY + dirBalleY * balle_vitesse) <  0){
+        dirBalleY = 1.2;
       }
-			else {
-				if ( ((balleY + dirBalleY * balle_vitesse) > (airzone_height - barre_height)) && ((balleX + dirBalleX * balle_vitesse) >= barreX) && ((balleX + dirBalleX * balle_vitesse) <= (barreX+barre_width))) {
-					dirBalleY = -1;
-					dirBalleX=2*(balleX-(barreX+barre_width/2))/barre_width;
-				}
-			}
-		}
+      else {
+        if ( ((balleY + dirBalleY * balle_vitesse) > (airzone_height - barre_height)) && ((balleX + dirBalleX * balle_vitesse) >= barreX) && ((balleX + dirBalleX * balle_vitesse) <= (barreX+barre_width))) {
+          dirBalleY = -1.2;
+          dirBalleX=2*(balleX-(barreX+barre_width/2))/barre_width;
+        }
+      }
+    }
     balleX+=dirBalleX*balle_vitesse;
     balleY+=dirBalleY*balle_vitesse;
-    // Colision avec les briques
-    if (balleY<=bricks){
-    var ligneY=Math.floor(balleY/(brick_height+space_brick));
-    var ligneX=Math.floor(balleX/(brick_width+space_brick));
-    if((tab_brick[ligneY][ligneX])==1){
-      // console.log(tab_brick[ligneY][ligneX]);
-    tab_brick[ligneY][ligneX]=0;
-    // console.log(tab_brick[ligneY][ligneX]);
-    dirBalleY=1;
-    }
-    }
+  }
+  function Balle(){
     //Creation de la balle
     context.fillStyle="white";
     context.beginPath();
@@ -108,31 +118,51 @@
     context.fill();
   }
 
+
+  function Colision(){
+    // Colision avec les briques
+    if (balleY<=bricks){
+      var ligneY=Math.floor(balleY/(brick_height+space_brick));
+      var ligneX=Math.floor(balleX/(brick_width+space_brick));
+      if((tab_brick[ligneY][ligneX])==1){
+        // console.log(tab_brick[ligneY][ligneX]);
+        tab_brick[ligneY][ligneX]=0;
+        // console.log(tab_brick[ligneY][ligneX]);
+        dirBalleY=1.5;
+      }
+    }
+  }
+
   // Fonction de gameOver
   function perdu(){
+    //PartieEnCours=true;
     clearInterval(Game);
     context.font="30px Helvetica Neue";
-    context.fillText("Game Over",180,300);
+    context.textAlign="center";
+    context.fillText("Game Over",airzone_width/2,airzone_height/2);
   }
 
   function gagne(){
+    //PartieEncours=true;
     clearInterval(Game);
-    alert("Win");
+    context.font="30px Helvetica Neue";
+    context.textAlign="center";
+    context.fillText("Win !",airzone_width/2,airzone_height/2);
+    aGagne=1;
   }
 
   // Fonction qui va créer des bricks
   function creation(ctx, nb_ligne, nb_brick_ligne, width, height, space) {
-
     aGagne=1;
     for (var i=0; i < nb_ligne; i++) {
       //On commence ici a initialiser
       tab_brick[i] = new Array(nb_brick_ligne);
-
+      //On remplit avec le tableau de couleur
       ctx.fillStyle=couleur[i];
       for (var j=0; j < nb_brick_ligne; j++) {
         //On affiche une nouvelle brique
         ctx.fillRect((j*(width+space)),(i*(height+space)),width,height);
-        //On dit qu'une brique existe si elle a comme valeur dans le tableau 1 donc 0 elle existe pas
+        //On dit qu'une brique existe si elle a comme valeur dans le tableau 1,donc, 0 elle existe pas
         tab_brick[i][j] = 1;
       }//endfor j
     }//endfor i
